949605368712cd6863bd0f12ffba53d059bc9fa9
Sergey Polichnoy <pilatuz@gmail.com>
2021-03-28 10:25:03 +0300

    add some helpers to "facade" package

--- uint128_facade.go
+++ uint128_facade.go
@@ -1,8 +1,27 @@
 package bigx
 
 import (
-	"github.com/Pilatuz/bigx/v2/uint128"
+	u128 "github.com/Pilatuz/bigx/v2/uint128"
 )
 
 // Uint128 is type alias for 128-bit unsigned integer.
-type Uint128 = uint128.Uint128
+type Uint128 = u128.Uint128
+
+// Note, there in no New(lo, hi) just not to confuse
+// which half goes first: lower or upper.
+// Use structure initialization Uint128{Lo: ..., Hi: ...} instead.
+
+// Zero128 is the lowest possible Uint128 value.
+func Zero128() Uint128 {
+	return u128.Zero()
+}
+
+// One128 is the lowest non-zero Uint128 value.
+func One128() Uint128 {
+	return u128.One()
+}
+
+// Max128 is the largest possible Uint128 value.
+func Max128() Uint128 {
+	return u128.Max()
+}
--- uint256_facade.go
+++ uint256_facade.go
@@ -1,8 +1,27 @@
 package bigx
 
 import (
-	"github.com/Pilatuz/bigx/v2/uint256"
+	u256 "github.com/Pilatuz/bigx/v2/uint256"
 )
 
 // Uint256 is type alias for 256-bit unsigned integer.
-type Uint256 = uint256.Uint256
+type Uint256 = u256.Uint256
+
+// Note, there in no New(lo, hi) just not to confuse
+// which half goes first: lower or upper.
+// Use structure initialization Uint256{Lo: ..., Hi: ...} instead.
+
+// Zero256 is the lowest possible Uint256 value.
+func Zero256() Uint256 {
+	return u256.Zero()
+}
+
+// One256 is the lowest non-zero Uint256 value.
+func One256() Uint256 {
+	return u256.One()
+}
+
+// Max256 is the largest possible Uint256 value.
+func Max256() Uint256 {
+	return u256.Max()
+}
